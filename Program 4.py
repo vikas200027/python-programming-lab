12d3a7b0c4fbc49b8640e9d7f5bf4e7be3f650b7
vikas200027 <106343443+vikas200027@users.noreply.github.com>
2022-05-27 22:29:42 +0530

    Add files via upload

---
+++ Program 4.py
@@ -0,0 +1,10 @@
+student_name = 'James'
+
+marks = {'James': 90, 'Jules': 55, 'Arthur': 77}
+
+for student in marks:
+    if student == student_name:
+        print(marks[student])
+        break
+else:
+    print('No entry with that name found.')
